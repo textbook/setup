2077dc662e0f1e1fe7546d9dc513eef794a5931f
Jonathan Sharpe <mail@jonrshar.pe>
2026-03-24 21:13:55 +0000

    Config for new repos

--- components/git.sh
+++ components/git.sh
@@ -11,7 +11,7 @@ git config --global diff.algorithm histogram
 git config --global diff.context 10
 git config --global fetch.prune true
 git config --global fetch.prunetags true
-git config --global init.defaultBranch main
+git config --global init.defaultbranch main
 git config --global log.date iso
 git config --global merge.conflictstyle zdiff3
 git config --global pull.rebase true
@@ -23,4 +23,5 @@ git config --global alias.co checkout
 git config --global alias.cp cherry-pick
 git config --global alias.ds "diff --staged"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.new '!git init && git commit --allow-empty --message "Initial commit"'
 git config --global alias.st status
